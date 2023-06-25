ad266a50ba07197341e8c7d9c71b72a4c0363b16
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2023-06-25 21:04:56 +0100

    feat: styled page template file

--- page.php
+++ page.php
@@ -3,9 +3,38 @@ get_header();
 while (have_posts()) {
     the_post();
     ?>
-    <h2>T is for title: <?php the_title() ?></h2>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title() ?></h1>
+        <div class="page-banner__intro">
+          <p>DON'T FORGET TO ADD TAGLINE LATER!</p>
+        </div>
+      </div>
+    </div>
 
-    <p><?php the_content(); ?></p>
+    <div class="container container--narrow page-section">
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+        </p>
+      </div>
+
+      <!-- <div class="page-links">
+        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <ul class="min-list">
+          <li class="current_page_item"><a href="#">Our History</a></li>
+          <li><a href="#">Our Goals</a></li>
+        </ul>
+      </div> -->
+
+      <div class="generic-content">
+        <p>
+            <?php the_content() ?>
+        </p>
+      </div>
+    </div>
+
     <?php
 }
 
