e4ee4a2e17c427a0fdfb73b99485e62589324cbc
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2023-06-28 07:32:08 +0100

    feat: responsive, lang and charset support

--- header.php
+++ header.php
@@ -1,4 +1,7 @@
-<head>
+<html <?php language_attributes( ) ?>>
+<head >
+    <meta charset="<?php bloginfo("charset") ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head() ?>
 </head>
 <body>
