dc6b85af4189c6d56c61e1366d9d2df8c61d0d9f
Tony Eneh <30419946+tony-eneh@users.noreply.github.com>
2023-07-09 05:02:28 +0100

    fix: highlight blog menu when on blog

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
               </li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
-              <li <?php if (is_page( 'blog' ) or wp_get_post_parent_id( 0 ) == 44 ) echo "class='current-menu-item'" ?>>
+              <li <?php if (get_post_type( ) == 'post' ) echo "class='current-menu-item'" ?>>
                 <a href="<?php echo site_url('/blog') ?>">Blog</a>
               </li>
             </ul>
